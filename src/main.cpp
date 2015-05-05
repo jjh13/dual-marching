328f0c8832d72f73effdbddebc0d3a25e769047f
jjh13 <joshua_horacsek@sfu.ca>
2015-05-04 18:22:17 -0700

    adding marschner lobb test function

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 #include <sisl/utility/ply_writer.hpp>
 
 #include <tuple>
+#include <cmath>
 
 #define VESION_STRING "0.1"
 
@@ -28,9 +29,37 @@ using namespace std;
 using namespace TCLAP;
 
 template <class T>
+class MarschnerLobb {
+public:
+	double a, f;
+
+	MarschnerLobb(double Fm, double alpha) : f(Fm), a(alpha){ }
+	double rho(double r){
+		return cos(2*M_PI*f*cos(r*M_PI/2.));
+	}	
+	double evaluate(const double &xx, const double &yy, const double &zz) {
+		double x = (xx-0.5)*2., y = (yy-0.5)*2., z = (zz-0.5)*2.;
+
+		double r = rho(sqrt(x*x + y*y));
+
+
+		double ret = 1. - sin(M_PI*z/2.) + a * (1. + r);
+
+		return ret/(2. + 2.*a);
+
+	}
+
+	double evaluate(const vector3<T> &p) {return this->evaluate(p.i, p.j, p.k);}
+	double div(const vector3<T> &p) { return this->div(p.i, p.j, p.k); }
+	double div(const double &x, const double &y, const double &z) {return 0;}
+	double laplacian(const vector3<T> &p){return this->laplacian(p.i, p.j, p.k);}
+	double laplacian(const double &x, const double &y, const double &z) {return 0;}
+};
+
+template <class T>
 class HamFunction {
 public:
-	HamFunction(){}	
+	HamFunction(){}
 	double evaluate(const double &x, const double &y, const double &z) {
 		// double xx = (x-0.5), yy = (y-0.5), zz = (z-0.5);
 		// return xx*xx + yy*yy + zz*zz - 0.25*0.25;
@@ -75,13 +104,14 @@ int main(int argc, char *argv[])
 		typedef linear_bcc_box<double, double> GFType;
 		typedef bcc_odd<linear_bcc_box<double, double>, double, double> LATType;
 
-		HamFunction<double> f;
-	
-		LATType *lat = new LATType(1./double(2.*100));
+		// HamFunction<double> f;
+		MarschnerLobb<double> f(6, 0.25);
+
+		LATType *lat = new LATType(1./double(2.*150));
 
 		lat->forEachLatticeSite([&](const int &i, const int &j, const int &k) {
 			vector3<double> p = lat->getSitePosition(i,j,k);
-			return f.evaluate(p);
+			return f.evaluate(p) - 0.5;
 		});
 
 		dualConour<double>(lat);
@@ -92,8 +122,8 @@ int main(int argc, char *argv[])
 			NULL, 
 			NULL, 
 			NULL, 
-			0., 
-			0.01, 
+			0, 
+			0.005, 
 			vector3<double>(0.1,0.1,0.1), 
 			vector3<double>(0.9,0.9,0.9));
 
@@ -239,7 +269,6 @@ void dualConour(bcc_odd<linear_bcc_box<T,T>, T, T>  *lattice){
 					coeff = ((value - 0)/(value - next_value));
 					pv = (vector3<T>(x,y,z) - vector3<T>(ii,jj,kk)).normalize();
 					pv = vector3<T>(ii,jj,kk) + pv * coeff;
-					printf("%f\n", coeff);
 
 					/*  */
 					std::vector<int> adj = adj_index[idx - 1];
